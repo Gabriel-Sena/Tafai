ea7e54e45db149414bf61996a8499b5294896d53
Gabriel Sena <gsdcdj@gmail.com>
2024-10-20 12:50:51 -0300

    Revert "tentando fazer menu mobile"
    This reverts commit 1cbb4008d3bbf09148c4ba8e28838eb6b9e0f3b3.

--- forms/scrips.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const sideMenu = document.getElementById('sideMenu');
-  if (sideMenu.style.left === '0px') {
-    sideMenu.style.left = '-250px'; // Ocultar menu
-  } else {
-    sideMenu.style.left = '0px'; // Mostrar menu
-  }
-}
